bee252f5f01080d3e5fdb69213980736f8235047
moshenghang; <844503861@qq.com;>
2020-10-14 20:38:54 +0800

    提交基础代码

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ic-gateway
-云卡网关
+这是云卡网关工程，用于签名、验签，控制流量，组装转发文
+swagger-bootstrap-ui访问地址
+http://localhost:${server.port}/${server.context-path}/doc.html
---
+++ src/main/java/com/shenhangyu/ic/gateway/service/CardInfoService.java
@@ -0,0 +1,25 @@
+/**
+ *版权所有©深航渔渔渔
+ */
+package com.shenhangyu.ic.gateway.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.shenhangyu.ic.gateway.bean.CardInfoBean;
+
+
+/**
+ * 描述:查询卡卡号信息接口
+ *
+ * @author moshenghang
+ * @create 2020/4/13 0013 20:15
+ */
+@Service
+public interface CardInfoService {
+	
+	public CardInfoBean getCardInfoBean(String userZhName);
+	
+    public List<CardInfoBean> queryICardInfos();
+}
